f0256a27763364c462238874bbe094c6f792405f
Padmanabh Kaushik <pkaushik0628@gmail.com>
2026-05-03 23:08:17 +1000

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -21,8 +21,8 @@ from pathlib import Path
 import torch
 from scipy.io import loadmat
 
-from Network2D import Network, N_E_ALEXNET, N_I_ALEXNET
-from HParams import HParams
+from src.Network2D import Network, N_E_ALEXNET, N_I_ALEXNET
+from src.HParams import HParams
 
 
 def load_images(path: str | Path, device: torch.device) -> torch.Tensor:
